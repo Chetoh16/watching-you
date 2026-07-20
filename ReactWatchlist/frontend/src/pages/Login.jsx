b8522a8e39ea2cda69a4d2ad5945d0a61c4eab11
Ege Cetin <egecetin1511@gmail.com>
2026-07-21 00:37:42 +0100

    feat: add boilerplate functions for login and signup

--- ReactWatchlist/frontend/src/pages/Login.jsx
+++ ReactWatchlist/frontend/src/pages/Login.jsx
@@ -1,3 +1,9 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useAuthContext } from '../contexts/AuthContext'
+
+function Login(){
+
+}
+
+export default Login
--- ReactWatchlist/frontend/src/pages/SignUp.jsx
+++ ReactWatchlist/frontend/src/pages/SignUp.jsx
@@ -1,3 +1,8 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useAuthContext } from '../contexts/AuthContext'
+
+function SignUp(){
+
+}
+export default SignUp
