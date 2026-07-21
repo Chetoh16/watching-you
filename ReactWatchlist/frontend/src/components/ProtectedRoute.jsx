6adca6207311c34284993fc58324c4bf55ec5f40
Ege Cetin <egecetin1511@gmail.com>
2026-07-21 01:05:27 +0100

    feat: add protected route so that the user has to be logged in to access pages

---
+++ ReactWatchlist/frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useAuthContext  } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+// wraps any route that requires login.
+// if no user, redirects to /login.
+// if logged in, renders the page as normal.
+function ProtectedRoute({ children }) {
+    const { user } = useAuthContext()
+
+    if (!user){
+        return <Navigate to="/login" replace />
+    } 
+    return children
+}
+
+export default ProtectedRoute
