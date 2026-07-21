1d7978bd7b4d5b769be960444adbab91cf48c5dc
Ege Cetin <egecetin1511@gmail.com>
2026-07-21 01:02:35 +0100

    chore: import css

--- ReactWatchlist/frontend/src/pages/Login.jsx
+++ ReactWatchlist/frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useAuthContext } from '../contexts/AuthContext'
+import '../css/Auth.css'
 
 function Login(){
 
