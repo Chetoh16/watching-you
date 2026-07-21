4118ddaceefab6ae3d3f60e3a850af541f149576
Ege Cetin <egecetin1511@gmail.com>
2026-07-21 01:07:58 +0100

    feat: wrap app in authprovider to allow users

--- ReactWatchlist/frontend/src/App.jsx
+++ ReactWatchlist/frontend/src/App.jsx
@@ -6,22 +6,41 @@ import NavBar from './components/NavBar'
 import { MovieProvider } from './contexts/MovieContext'
 import Watchlists from './pages/Watchlists'
 import WatchlistDetail from './pages/WatchlistDetail'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import ProtectedRoute from './components/ProtectedRoute'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 
 
   return (
-    <MovieProvider> 
-      <NavBar/>
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/favourites" element={<Favourites/>}/>
-          <Route path="/watchlists" element={<Watchlists/>}/>
-          <Route path="/watchlists/:watchlistId" element={<WatchlistDetail/>}/>
-        </Routes> 
-      </main>
-    </MovieProvider>
+    // AuthProvider is outside MovieProvider because MovieContext
+    // needs to know who's logged in to fetch the right data
+    <AuthProvider>
+        <MovieProvider>
+            <NavBar/>
+            <main className="main-content">
+                <Routes>
+                    {/* Public routes which anyone can access */}
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/signup" element={<SignUp/>}/>
+                    <Route path="/" element={<Home/>}/>
+
+                    {/* Protected routes which must be logged in to access */}
+                    <Route path="/favourites" element={
+                        <ProtectedRoute><Favourites/></ProtectedRoute>
+                    }/>
+                    <Route path="/watchlists" element={
+                        <ProtectedRoute><Watchlists/></ProtectedRoute>
+                    }/>
+                    <Route path="/watchlists/:watchlistId" element={
+                        <ProtectedRoute><WatchlistDetail/></ProtectedRoute>
+                    }/>
+                </Routes>
+            </main>
+        </MovieProvider>
+    </AuthProvider>
   )
 }
 
