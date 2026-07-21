06479fa3af2e47f667a0d30d7ab4634fdf0dcea1
Ege Cetin <egecetin1511@gmail.com>
2026-07-21 02:53:25 +0100

    refactor: replace localstorage with supabase functions in favourites

--- ReactWatchlist/frontend/src/pages/Favourites.jsx
+++ ReactWatchlist/frontend/src/pages/Favourites.jsx
@@ -1,33 +1,46 @@
-import "../css/Favourites.css"
-import { useMovieContext } from "../contexts/MovieContext"
-import MovieCard from "../components/MovieCard"
+import { useState, useEffect } from 'react'
+import { useMovieContext } from '../contexts/MovieContext'
+import { getMovieById } from '../services/api'
+import MovieCard from '../components/MovieCard'
+import '../css/Favourites.css'
 
 function Favourites(){
     const {favourites} = useMovieContext();
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
 
-    if (favourites && favourites.length > 0) {
-        return (
+    useEffect(() => {
+        if (favourites.length === 0) {
+            setMovies([])
+            setLoading(false)
+            return
+        }
+        Promise.all(favourites.map(id => getMovieById(id)))
+            .then(results => {
+                setMovies(results.filter(Boolean))
+                setLoading(false)
+            })
+    }, [favourites])
+
+    if (loading) return <div className="favourites-empty"><p>Loading...</p></div>
+
+    if (movies.length === 0) return (
+        <div className="favourites-empty">
+            <h2> No Favourite Movies Yet</h2>
+            <p> Start adding movies.</p>
+        </div>
+    )
+
+    return (
         <div className="favourites">
-                <h2> Your Favourites </h2>
-                <div className="movies-grid">
-                {favourites.map((movie) => (
-                    (
-                        <MovieCard movie={movie} key={movie.id} />
-                    )
+            <h2>Your Favourites</h2>
+            <div className="movies-grid">
+                {movies.map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
         </div>
-        ); 
-    }
-    
-
-    return <div className="favourites-empty">
-        <h2> No Favourite Movies Yet</h2>
-        <p> Start adding movies.</p>
-    </div>
-
-
-
+    )
 }
 
 export default Favourites
